550ef7979bfb91a537801ab69afb82ac4941fad5
unknown <YY227@ccs.local>
2025-02-05 05:16:32 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!'); 
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+app.use(express.json()); 
+
+app.post('/submit', (req, res) => {
+  const data = req.body;
+  res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
