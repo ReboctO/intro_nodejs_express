224960cd7a59509aa7816449b26632847798b808
unknown <HH826@ccs.local>
2025-02-06 21:06:33 +0800

    second

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ app.post('/submit', (req, res) => {
   res.send(`Received: ${JSON.stringify(data)}`);
 });
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+  const newItem = req.body.item;
+  items.push(newItem);
+  res.json(items);
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
